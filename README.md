47ea32a33601099a015c5622098ab37c54fa2764
divyap76 <111829117+divyap76@users.noreply.github.com>
2022-12-11 22:50:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic-angular-create-new-account-form
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6mxzyn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  NAME: string;
+  heading: string = 'create new account from ts';
+
+constructor(private fb: FormBuilder){}
+
+createNewProfile:FormGroup;
+
+ngOnInit() {
+    this.createNewProfile=this.fb.group({
+name:[""],
+age:[],
+sex:[""],
+dateOfBirth:[],
+postalCode:[],
+city:[""],
+district:[""],
+state:[""],
+country:[""],
+emailId:[""],
+username:[""],
+password:[""],
+clgName:[""],    
+})
+}
+
+login(){
+  console.log("login clicked");
+console.log("form controls",  this.createNewProfile);
+this.createNewProfile.controls.name.setValue("Tamilarasu");
+}
+
+  changeTitle(){
+    this.heading="heading changed";
+  }
+
+}
